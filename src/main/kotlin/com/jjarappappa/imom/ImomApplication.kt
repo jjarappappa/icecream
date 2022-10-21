309388e21af60b6db22d2b662cdd4ab1fcbe4ca0
leekyukin <leekujin14@gmail.com>
2022-10-21 11:12:40 +0900

    DELETE :: PropertiesConfig
    - @ConfigurationProperties를 여러 곳에서  사용하려면@ConfigurationPropertiesScan을 application.kt로 옮겨야함

--- src/main/kotlin/com/jjarappappa/imom/ImomApplication.kt
+++ src/main/kotlin/com/jjarappappa/imom/ImomApplication.kt
@@ -1,9 +1,11 @@
 package com.jjarappappa.imom
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class ImomApplication
 
 fun main(args: Array<String>) {
