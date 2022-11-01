c133768095ac828cba073209d2d786d0806c52e8
gimhanul <gimhanuly@gmail.com>
2022-11-01 11:09:23 +0900

    REFACTOR :: UserErrorProperty (#7)

--- src/main/kotlin/com/jjarappappa/imom/domain/user/exception/EmailAlreadyExistsException.kt
+++ src/main/kotlin/com/jjarappappa/imom/domain/user/exception/EmailAlreadyExistsException.kt
@@ -1,8 +1,9 @@
 package com.jjarappappa.imom.domain.user.exception
 
+import com.jjarappappa.imom.domain.user.exception.error.UserErrorProperty
 import com.jjarappappa.imom.global.error.exception.ErrorCode
 import com.jjarappappa.imom.global.error.exception.IMomException
 
-object EmailAlreadyExistsException: IMomException(ErrorCode.EMAIL_ALREADY_EXISTS) {
+object EmailAlreadyExistsException: IMomException(UserErrorProperty.EMAIL_ALREADY_EXISTS) {
     val EXCEPTION by lazy { this }
 }
--- src/main/kotlin/com/jjarappappa/imom/domain/user/exception/PhoneNumberAlreadyExistsException.kt
+++ src/main/kotlin/com/jjarappappa/imom/domain/user/exception/PhoneNumberAlreadyExistsException.kt
@@ -1,8 +1,9 @@
 package com.jjarappappa.imom.domain.user.exception
 
+import com.jjarappappa.imom.domain.user.exception.error.UserErrorProperty
 import com.jjarappappa.imom.global.error.exception.ErrorCode
 import com.jjarappappa.imom.global.error.exception.IMomException
 
-object PhoneNumberAlreadyExistsException: IMomException(ErrorCode.PHONE_NUMBER_ALREADY_EXISTS) {
+object PhoneNumberAlreadyExistsException: IMomException(UserErrorProperty.PHONE_NUMBER_ALREADY_EXISTS) {
     val EXCEPTION by lazy { this }
 }
