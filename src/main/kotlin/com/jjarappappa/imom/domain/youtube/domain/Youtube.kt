c8cb324b8b7ca24f58acd8587580b769b5646f9c
leekyukin <leekujin14@gmail.com>
2022-11-22 12:11:22 +0900

    fix: 자잘한 에러

--- src/main/kotlin/com/jjarappappa/imom/domain/youtube/domain/Youtube.kt
+++ src/main/kotlin/com/jjarappappa/imom/domain/youtube/domain/Youtube.kt
@@ -2,8 +2,12 @@ package com.jjarappappa.imom.domain.youtube.domain
 
 import com.jjarappappa.imom.global.entity.BaseTimeEntity
 import javax.persistence.Column
+import javax.persistence.Entity
 import javax.persistence.Id
+import javax.persistence.Table
 
+@Entity
+@Table(name = "tbl_youtube")
 class Youtube(
     @Column(length = 50, nullable = false)
     var title: String,
