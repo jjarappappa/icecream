d897b9cbba9d08fdc5dd5646dd76d9268cec5e3e
leekyukin <leekujin14@gmail.com>
2022-11-24 16:52:17 +0900

    ADD :: 컬럼 세부사항 정의

--- src/main/kotlin/com/jjarappappa/imom/domain/information/domain/Information.kt
+++ src/main/kotlin/com/jjarappappa/imom/domain/information/domain/Information.kt
@@ -1,25 +1,16 @@
-package com.jjarappappa.imom.domain.information
+package com.jjarappappa.imom.domain.information.domain
 
 import com.jjarappappa.imom.domain.user.domain.User
 import com.jjarappappa.imom.global.entity.BaseTimeEntity
-import java.lang.ProcessHandle.Info
-import javax.persistence.Entity
-import javax.persistence.Enumerated
-import javax.persistence.ManyToOne
-import javax.persistence.Table
-import javax.persistence.JoinColumn
-import javax.persistence.EnumType
-import javax.persistence.FetchType
-import javax.persistence.CascadeType
-import javax.persistence.ElementCollection
-import javax.persistence.Id
-import javax.persistence.GenerationType
-import javax.persistence.GeneratedValue
+import org.hibernate.validator.constraints.Length
+import javax.persistence.*
 
 @Entity
 @Table(name = "tbl_information")
 class Information(
+    @Length(min = 3, max = 20)
     var title: String,
+    @Length(min = 1, max = 1500)
     var content: String,
 
     @Enumerated(EnumType.STRING)
