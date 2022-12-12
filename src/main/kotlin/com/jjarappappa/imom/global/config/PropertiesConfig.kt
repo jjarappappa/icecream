e3f9b1757657b6410324ff76bbddd437786ad10d
gimhanul <gimhanuly@gmail.com>
2022-12-12 16:55:39 +0900

    FIX :: cors

--- src/main/kotlin/com/jjarappappa/imom/global/config/PropertiesConfig.kt
+++ src/main/kotlin/com/jjarappappa/imom/global/config/PropertiesConfig.kt
@@ -1,6 +1,5 @@
 package com.jjarappappa.imom.global.config
 
-import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.Configuration
 
--- src/main/kotlin/com/jjarappappa/imom/global/security/SecurityConfig.kt
+++ src/main/kotlin/com/jjarappappa/imom/global/security/SecurityConfig.kt
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsUtils
 
 @Configuration
 @EnableWebSecurity
@@ -41,6 +42,7 @@ class SecurityConfig(
 
             .and()
             .authorizeRequests()
+            .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
             .anyRequest().permitAll()
 
         http
