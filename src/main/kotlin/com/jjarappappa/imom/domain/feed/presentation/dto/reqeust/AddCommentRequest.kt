e20dbfa1f4c451a97bbe4e2704b1816677e246c3
gimhanul <gimhanuly@gmail.com>
2022-12-13 19:56:04 +0900

    CHPRE :: 에헷쿵 ㅋㅋ;

--- src/main/kotlin/com/jjarappappa/imom/domain/feed/presentation/dto/reqeust/AddCommentRequest.kt
+++ src/main/kotlin/com/jjarappappa/imom/domain/feed/presentation/dto/reqeust/AddCommentRequest.kt
@@ -1,11 +1,11 @@
 package com.jjarappappa.imom.domain.feed.presentation.dto.reqeust
 
-import javax.validation.constraints.Max
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
 
 data class AddCommentRequest(
 
     @field:NotBlank
-    @field:Max(200)
+    @field:Size(max = 200)
     var comment: String,
 )
--- src/main/kotlin/com/jjarappappa/imom/domain/feed/presentation/dto/reqeust/UpdateFeedRequest.kt
+++ src/main/kotlin/com/jjarappappa/imom/domain/feed/presentation/dto/reqeust/UpdateFeedRequest.kt
@@ -4,15 +4,16 @@ import com.jjarappappa.imom.domain.feed.domain.type.FeedType
 import javax.validation.constraints.Max
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
 
 data class UpdateFeedRequest(
 
     @field:NotBlank
-    @field:Max(30)
+    @field:Size(max = 30)
     val title: String,
 
     @field:NotBlank
-    @field:Max(2000)
+    @field:Size(max = 2000)
     val content: String,
 
     @field:NotNull
